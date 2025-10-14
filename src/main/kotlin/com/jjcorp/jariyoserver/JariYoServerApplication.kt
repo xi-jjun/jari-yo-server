b541c35b27413485af642bea9c5d133580db3fcd
Kim Jae Jun <rlawowns97@naver.com>
2025-10-15 03:30:25 +0900

    init project

---
+++ src/main/kotlin/com/jjcorp/jariyoserver/JariYoServerApplication.kt
@@ -0,0 +1,11 @@
+package com.jjcorp.jariyoserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JariYoServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<JariYoServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/jjcorp/jariyoserver/JariYoServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jjcorp.jariyoserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JariYoServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
